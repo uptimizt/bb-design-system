1820169738625b24316b94157e17ea8d07a100e9
uptimizt <uptimizt@gmail.com>
2022-01-30 11:28:30 +0300

    up

---
+++ app.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+* Plugin Name: BB Design System
+* Description: Block-base design system for WordPress, Gutenberg and Full site editing
+* Version: 1.20220130
+*/
+
+add_action('plugins_loaded', function(){
+    
+});
