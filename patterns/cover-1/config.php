d67a3b09b1986a795279782abbd9c454034b4858
uptimizt <uptimizt@gmail.com>
2022-01-30 12:55:48 +0300

    up

--- patterns/cover-1/config.php
+++ patterns/cover-1/config.php
@@ -1,11 +1,12 @@
-<?php 
+<?php
 
-add_action( 'init', function(){
+add_action('init', function () {
 
     $args = [
         'title'       => 'Cover 1',
         'description' => 'The main cover image',
-        'categories'  => [ 'bbds', 'text' ],
+        'categories'  => ['bbds', 'text'],
+        'keywords'    => ['cover', 'head', 'title'],
         'content'     => '',
     ];
 
@@ -15,12 +16,6 @@ add_action( 'init', function(){
     include_once $path;
     $pattern = ob_get_clean();
 
-    // echo '<pre>';
-    // var_dump($pattern); exit;
-    // $args['content'] = '<!-- wp:paragraph --><p>A single paragraph block style</p><!-- /wp:paragraph -->';
-    $args['content'] = wp_kses_post( $pattern );
-    register_block_pattern( 'bbds/' . $slug, $args );
-
-    // var_dump($dd); exit;
-
+    $args['content'] = wp_kses_post($pattern);
+    register_block_pattern('bbds/' . $slug, $args);
 });
