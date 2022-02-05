55e817d8ad3ca1f78e514adc42624b06ba6a8b8e
uptimizt <uptimizt@gmail.com>
2022-02-05 13:59:34 +0300

    up

--- app.php
+++ app.php
@@ -76,49 +76,11 @@ function add_mods_assets(){
         }
         $file_url = plugins_url('mods/' . $dir_name . '/style.css', __FILE__);
         $file_version = filemtime($file_path);
-//         $block_css_version = filemtime($css_file_path);;
 
         wp_enqueue_style($dir_name . '-style', $file_url, [], $file_version);
 
     }
 }
-// function backend()
-// {
-
-//     foreach (self::get_directories() as $dir_name) {
-//         $dir_path = sprintf('%s/lazyblock/%s', __DIR__, $dir_name);
-//         if (!is_dir($dir_path)) {
-//             continue;
-//         }
-
-//         $css_file_path = sprintf('%s/block.css', $dir_path);
-
-//         if (!file_exists($css_file_path)) {
-//             continue;
-//         }
-//         $block_css_url = plugins_url(sprintf('lazyblock/%s/block.css', $dir_name), __FILE__);
-//         $block_css_version = filemtime($css_file_path);;
-
-//         wp_enqueue_style($dir_name . '-style', $block_css_url, ['wp-edit-blocks'], $block_css_version);
-//     }
-// }
-
-
-// function commone_style()
-// {
-//     $path = 'lazyblock/style.css';
-//     $url = plugins_url($path, __FILE__);
-//     $path = __DIR__ . '/' . $path;
-//     if (file_exists($path)) {
-//         wp_enqueue_style(
-//             'style-lbu7',
-//             $url,
-//             $dep = [],
-//             $var = filemtime($path)
-//         );
-//     }
-// }
-
 
 
 function get_directories($subdir = 'patterns')
